e60980e05a9ac797ff26d8696fcd2ab276fa99c4
Sunny Lee <sunny121@uw.edu>
2021-04-12 01:05:37 -0700

    Almost done2

--- app/src/main/java/edu/uw/sunny121/dotify/MainActivity.kt
+++ app/src/main/java/edu/uw/sunny121/dotify/MainActivity.kt
@@ -4,11 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
-import java.util.*
+import android.widget.*
+import androidx.core.view.isVisible
+import org.w3c.dom.Text
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -17,16 +15,40 @@ class MainActivity : AppCompatActivity() {
     private var randomNumber = Random.nextInt(1000, 10000)
     private lateinit var ivPrev: ImageView
     private lateinit var ivNext: ImageView
+    private lateinit var etUserName : EditText
+    private lateinit var tvUserName : TextView
+    private lateinit var button : Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tvNumberSongs = findViewById<TextView>(R.id.tvNumberSongs)
+        tvNumberSongs = findViewById(R.id.tvNumberSongs)
 
         tvNumberSongs.text = "$randomNumber plays"
 
         ivPrev = findViewById(R.id.ivPrev)
         ivNext = findViewById(R.id.ivNext)
+        etUserName = findViewById(R.id.etUserName)
+        tvUserName = findViewById(R.id.tvUserName)
+        button = findViewById(R.id.button)
+
+        etUserName.visibility = View.GONE
+
+        button.setOnClickListener{
+            if(button.text.toString() == "Apply") {
+                val userInputtedText = etUserName.text.toString()
+                tvUserName.text = userInputtedText
+                etUserName.visibility = View.GONE
+                tvUserName.visibility = View.VISIBLE
+                button.text = "Change user"
+            } else {
+                etUserName.visibility = View.VISIBLE
+                tvUserName.visibility = View.GONE
+                button.text = "Apply"
+            }
+
+        }
 
         ivPrev.setOnClickListener{
             prevButtonClicked()
@@ -44,7 +66,7 @@ class MainActivity : AppCompatActivity() {
     
     fun prevButtonClicked() {
         Toast.makeText(this,"Skipping to previous track", Toast.LENGTH_SHORT).show()
-        Log.i("echhe", "clicked prev")
+        //Log.i("echhe", "clicked prev")
     }
 
     fun nextButtonClicked() {
